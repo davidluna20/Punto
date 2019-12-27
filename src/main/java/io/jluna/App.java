2c22689ad08c6bff2c08b9c5d1aea2e7abccc2cb
jomosquera <david.es1005@gmail.com>
2019-12-26 20:38:23 -0500

    Se agregan los archivos base del proyecto.

---
+++ src/main/java/io/jluna/App.java
@@ -0,0 +1,13 @@
+package io.jluna;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
